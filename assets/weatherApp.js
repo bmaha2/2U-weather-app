d96541d7b16884df176c378d36f63af7e0bbbc05
Ian Deans <ideans715@gmail.com>
2020-04-08 12:22:49 -0700

    add code for updating current weather data on DOM

--- assets/weatherApp.js
+++ assets/weatherApp.js
@@ -1,34 +1,14 @@
-/**
- * on load:
- *  user sees: input for searching by city name
- *             button at end of input for submitting search
- *             list of previous searches: when clicked submits that query again
- * 
- * 
- *  when user clicks submit:
- *      the city name is retrieved from input.
- *          input is validated as alphabetical characters
- *      the city name is saved in history if not already saved
- *      city name is passed to fetch function
- * 
- * fetch function:
- *  - get lat & lon
- *  - get all weather data for city
- *  - extract required data from response data
- *  - update dom with data: updateDOM();
- * 
- * updateDOM:
- *  - clear out any previous elements in containers: current-weather-container, forecast-container
- *  
- */
-$(document).ready(() => {
+
+$( document ).ready( () => {
 
     const domRefs = {
-        cityNameInput: $(".search-input"),
+        cityNameInput: $( ".search-input" ),
+        currentWeather: $( ".current-weather-area" ),
     };
 
     const SCHEDULE_DATA = "SCHEDULE_DATA";
-    const API_KEY = "8fa0090fd319eb6475948e3790aed3fd";
+    const API_KEY = "f41bacc17b33c9263b7d935d88070b40";
+    const DATE_FORMAT = "MMM D"
 
 
     addListeners()
@@ -39,18 +19,19 @@ $(document).ready(() => {
         return domRefs.cityNameInput.val();
     }
 
-    function saveCityName(cityName) {
+    function saveCityName( cityName ) {
         const scheduleData = loadScheduleData();
-        if (!scheduleData.includes(cityName)) {
-            scheduleData.push(cityName);
-            saveScheduleData(scheduleData);
+        if ( !scheduleData.includes( cityName ) ) {
+            scheduleData.push( cityName );
+            saveScheduleData( scheduleData );
         }
     }
 
     function loadScheduleData() {
-        let data = localStorage.getItem(SCHEDULE_DATA);
+        let data = localStorage.getItem( SCHEDULE_DATA );
 
         // If Else Example
+
         // if (data) {
         //     data = JSON.parse(data);
         // } else {
@@ -59,103 +40,119 @@ $(document).ready(() => {
         // return data;
 
         // Ternary Example
-        return data ? JSON.parse(data) : []
+        return data ? JSON.parse( data ) : []
     }
 
-    function saveScheduleData(scheduleData) {
+    function saveScheduleData( scheduleData ) {
         localStorage
-            .setItem(SCHEDULE_DATA, JSON.stringify(scheduleData))
+            .setItem( SCHEDULE_DATA, JSON.stringify( scheduleData ) )
     }
 
-    function generateCurrentWeatherCard(weatherData) {
-        const $card = $("<div class='current-weather-card'>")
+    function generateCurrentWeatherCard( { date, temp, humidity, uvi, icon, windSpeed } ) {
+        const $card = $( "<div class='current-weather-card'>" )
 
-        const $date = generateCardRow("", weatherData.date);
-        const $temp = generateCardRow("Temperature: ", weatherData.temp);
-        const $humidity = generateCardRow("Humidity: ", weatherData.humidity);
-        const $windspeed = generateCardRow("Wind Speed: ", weatherData.windspeed);
-        const $uvIndex = generateCardRow("UV Index: ", weatherData.uvIndex);
+        const $date = generateCardRow( "", date );
+        const $temp = generateCardRow( "Temperature: ", temp );
+        const $humidity = generateCardRow( "Humidity: ", humidity );
+        const $windspeed = generateCardRow( "Wind Speed: ", windSpeed );
+        const $uvIndex = generateCardRow( "UV Index: ", uvi );
 
-        $card.append($date, $temp, $humidity, $windspeed, $uvIndex);
+        $card.append( $date, $temp, $humidity, $windspeed, $uvIndex );
 
         return $card;
     }
 
-    function generateCardRow(label, row) {
-        const $row = $("<div>").text(label);
-        const $value = $("<span>").text(value);
-        $row.append($value);
+    function generateCardRow( label, value ) {
+        const $row = $( "<div>" ).text( label );
+        const $value = $( "<span>" ).text( value );
+        $row.append( $value );
+        return $row;
     }
 
-    function generateForecastCard(data) {
-        const $img = $("<img>").attr("src", data.iconURL);
-        const $temp = $("<div>").text(data.temp);
-        const $humidity = $("<div>").text(data.humidity);
+    function generateForecastCard( data ) {
+        const $img = $( "<img>" ).attr( "src", iconURL( data.iconURL ) );
+        const $temp = $( "<div>" ).text( data.temp );
+        const $humidity = $( "<div>" ).text( data.humidity );
 
-        const $card = $("<div class='forecast-card'>");
-        $card.append($img, $temp, $humidity);
+        const $card = $( "<div class='forecast-card'>" );
+        $card.append( $img, $temp, $humidity );
         return $card;
     }
 
-    function generateForecastCards(forecastArray) {
-        const $container = $("<div class='forecast-container'>");
-        const $cards = forecastArray.map(day => generateForecastCard(day));
-        $container.append(...$cards);
+    function generateForecastCards( forecastArray ) {
+        const $container = $( "<div class='forecast-container'>" );
+        const $cards = forecastArray.map( day => generateForecastCard( day ) );
+        $container.append( ...$cards );
         return $container;
     }
 
     function addListeners() {
-        $(".search-btn").on("click", handleClickSearch);
+        $( ".search-btn" ).on( "click", handleClickSearch );
 
     }
 
-    function handleClickSearch(event) {
+    function handleClickSearch( event ) {
         event.preventDefault();
-        console.log("Click")
         const cityName = getCityNameFromInput();
-        console.log(cityName)
-        saveCityName(cityName);
-
-        getWeatherData(cityName);
+        saveCityName( cityName );
+        getWeatherData( cityName );
     }
 
-    function getWeatherData(cityName) {
-        // get lon lat, get weather data, create cards, append cards
-        console.log('get weather data')
+    function getWeatherData( cityName ) {
 
-        fetchLatAndLon(cityName)
-            // .then( fetchWeatherData )
-            // .then( response => {
-            //     console.log(response)
-            // })
+        fetchLatAndLon( cityName )
+            .then( fetchWeatherData )
+            .then( ( { current, daily } ) => {
+                const currentWeatherCard = generateCurrentWeatherCard( extractCurrentWeatherData( current ) )
+                updateCurrentWeather( currentWeatherCard )
+
+                console.log( daily )
+                extractForecastData( daily );
+
+            } )
+            .catch( err => console.error( err ) );
+    }
+
+    function extractForecastData( dailyData ) {
+        const fiveDays = [];
+        for ( let i=0; i < 5; i++) {
+            fiveDays.push( dailyData[i] );
+        }
+        return fiveDays;
+    }
+
+    function updateCurrentWeather( card ) {
+        domRefs.currentWeather.empty();
+        domRefs.currentWeather.append( card );
     }
 
     function fetchLatAndLon( cityName ) {
-        console.log('fetching lat and lon for ', cityName)
-        const url = latAndLonURL( cityName )
-        console.log( url );
-        return $.ajax({
-            url: url,
+        return $.ajax( {
+            url: latAndLonURL( cityName ),
             method: "GET",
-        })
-        .then( resp => console.log( resp ) )
-        .catch( error => console.error( error ))
+        } )
     }
 
-    function fetchWeatherData({ coords }) {
-        console.log('weather data fetch')
-        console.log(coords)
-        return $.ajax({
-            url: weatherDataURL( coords ),
+    function fetchWeatherData( { coord } ) {
+        return $.ajax( {
+            url: weatherDataURL( coord ),
             method: "GET",
-        })
+        } )
     }
 
-    function latAndLonURL(cityName) {
-        return `https://api.openweathermap.org/data/2.5/weather?id=${cityName}&appid=${API_KEY}`
+    function extractCurrentWeatherData( { dt, temp, humidity, uvi, weather: [ { icon } ], wind_speed: windSpeed } ) {
+        return { date: moment.unix( dt ).format( DATE_FORMAT ), temp, humidity, uvi, icon, windSpeed }
     }
 
-    function weatherDataURL({ lat, lon }) {
-        return `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}appid=${ API_KEY } `;
-}
-})
+    function latAndLonURL( cityName ) {
+        return `https://api.openweathermap.org/data/2.5/weather?q=${ cityName }&appid=${ API_KEY }`
+    }
+
+    function weatherDataURL( { lat, lon } ) {
+        return `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&units=imperial&appid=${ API_KEY } `;
+    }
+
+    function iconURL( icon ) {
+        return `http://openweathermap.org/img/wn/${ icon }@2x.png`;
+    }
+} )
